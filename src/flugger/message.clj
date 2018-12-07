a054c4739aedcc46ee69fefded31106d2bee8457
Valerij Stukanov <vstukanov@me.com>
2018-12-07 10:02:08 +0200

    RPC messages encoding/decoding.

--- src/flugger/message.clj
+++ src/flugger/message.clj
@@ -1,12 +1,28 @@
 (ns flugger.message
-  (:import (flugger.protobuf RPC$Request))
-  (:require [clojure.java.io :refer [input-stream]]
+  (:import (flugger.protobuf RPC$Request
+                             RPC$AuthRequest))
+  (:require [clojure.java.io :refer [input-stream output-stream]]
             [protobuf.core :as protobuf]))
 
 (defn text->stream [text]
   (input-stream (.getBytes text)))
 
-(defn unmarshall [msg]
-  (let [msg-stream (text->stream msg)
-        empty-request (protobuf/create RPC$Request)]
-    (protobuf/read empty-request message-stream)))
+(defn msg->bytes [type data]
+  (-> (protobuf/create type data)
+      protobuf/->bytes))
+
+(defn request->bytes [req-meta type args]
+  (let [args-bytes (msg->bytes type args)
+        req-data (assoc req-meta :args args-bytes)]
+    (msg->bytes RPC$Request req-data)))
+
+(defn bytes->msg [bytes type]
+  (let [req (protobuf/create type)]
+    (protobuf/bytes-> req bytes)))
+
+(defn bytes->request [bytes arg-type-resolver]
+  (let [req (bytes->msg bytes RPC$Request)
+        args-type (arg-type-resolver (:call req))
+        args-bytes (-> req :args .toByteArray)
+        args (bytes->msg args-bytes args-type)]
+    (merge {} req {:args args})))
